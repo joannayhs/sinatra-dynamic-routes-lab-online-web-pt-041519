dd3f83a51c710b75aee84d0f3aceba96870bff67
Joanna Yuhas <joannayuhas@gmail.com>
2019-07-24 18:56:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,9 +16,9 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do 
     num = params[:number].to_i 
     phrase = params[:phrase] 
-    num.times do
-      "#{phrase}"
-    end 
+   answer =  num.times do
+              "#{phrase}"
+              end 
   end 
-  
+  answer
 end
