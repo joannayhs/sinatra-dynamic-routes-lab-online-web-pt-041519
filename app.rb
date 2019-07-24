40c55ecf3893379b64bdb5850c9201bbab17864e
Joanna Yuhas <joannayuhas@gmail.com>
2019-07-24 19:20:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,8 @@ class App < Sinatra::Base
   end
   
   get "/say/:word1/:word2/:word3/:word4/:word5" do 
-    params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
+    words = [] 
+    words << params[:word1]
   end 
 
 end
